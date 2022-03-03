b9716bd5d4b0ab897e5ce708b8bde36cc763651c
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-02 17:18:54 -0800

    Add project files.

---
+++ SLMBugTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SLMBugTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
