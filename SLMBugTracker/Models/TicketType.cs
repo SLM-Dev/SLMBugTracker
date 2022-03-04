2094a2ca2f142770f46706deab21c3d5c7a75242
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-04 11:22:22 -0800

    ✨ Add Ticket Type model 👔 Add names Id of tickets

---
+++ SLMBugTracker/Models/TicketType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SLMBugTracker.Models
+{
+    public class TicketType
+    {
+        public int Id { get; set; }
+        [DisplayName("Type Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+
+}
