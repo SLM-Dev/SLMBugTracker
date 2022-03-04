a550d0dff6f09b6069ab8b3b10f44ae5771b820b
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-03 23:09:11 -0800

    🗃️ Add postgres migrations database 👔 Add names to login ✨Add display full name once login

--- SLMBugTracker/Data/Migrations/20220304063914_Initial_001.Designer.cs
+++ SLMBugTracker/Data/Migrations/20220304063914_Initial_001.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SLMBugTracker.Data;
@@ -9,9 +10,10 @@ using SLMBugTracker.Data;
 namespace SLMBugTracker.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220304063914_Initial_001")]
+    partial class Initial_001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
