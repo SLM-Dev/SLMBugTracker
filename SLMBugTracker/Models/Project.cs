2cca72192f1baac4f2d0f363f1ea37cba503fb58
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-06 00:46:25 -0800

    ✨ Add project, project priority model with data 👔 Add foreign keys for project, project priority 👔Add navigation properties for project model ✨Add iformfile for uploading files 👔Add data annotations to project, project priority 🗃️👔Add collection for tickets and members for project model 👔Add primary key for project priority ✨Add component model for project, project priority

---
+++ SLMBugTracker/Models/Project.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SLMBugTracker.Models
+{
+    public class Project
+    {
+        //Primary Key
+        public int Id { get; set; }
+
+        [DisplayName("Company")]
+
+        // foreign key for Company
+        public int? CompanyId { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Project Name")]
+        public string Name { get; set; }
+
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+
+        //Start Date
+        [DataType(DataType.Date)]
+        [DisplayName("Start Date")]
+        public DateTimeOffset StartDate { get; set; }
+
+        //End Date
+        [DataType(DataType.Date)]
+        [DisplayName("End Date")]
+        public DateTimeOffset EndDate { get; set; }
+
+        //Priority
+        [DisplayName("Priority")]
+        public int ProjectPriorityId { get; set; }
+
+        //set the ImageFormFile property to a new instance of the IFormFile class.
+
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFormFile { get; set; }
+        [DisplayName("File Name")]
+ 
+        public string ImageUrl { get; set; }
+
+        public byte[] ImageFileData { get; set; }
+        
+        [DisplayName("File Extension")]
+        public string ImageContentType { get; set; }
+
+        [Display(Name = "Archived")]
+        public bool Archived { get; set; }
+
+
+
+        //Navigation Properties
+        public virtual Company Company { get; set; }
+
+        // Project Priority
+        public virtual ProjectPriority ProjectPriority { get; set; }
+
+        public virtual ICollection<BTUser> Members { get; set; } = new 
+        HashSet<BTUser>();
+        public virtual ICollection<Ticket> Tickets { get; set; } = new 
+        HashSet<Ticket>();
+        
+
+
+
+       
+    
+    }
+}
---
+++ SLMBugTracker/Models/ProjectPriority.cs
@@ -0,0 +1,21 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SLMBugTracker.Models
+{
+    public class ProjectPriority
+    {
+        //Primary Key
+        public int Id { get; set; }
+
+        // [Required]
+        // [StringLength(50)]
+        [DisplayName("Priority")]
+        public string Name { get; set; }
+
+    
+    }
+}
