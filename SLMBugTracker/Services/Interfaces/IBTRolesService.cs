596467efca16a6751b2986189669c66b93282a0a
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-09 10:52:18 -0800

    👔  Add interface for  bt roles service

--- SLMBugTracker/Services/Interfaces/IBTRolesService.cs
+++ SLMBugTracker/Services/Interfaces/IBTRolesService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace SLMBugTracker.Services.Interfaces
 {
     public interface IBTRolesService
-    {
+    {  
      public Task<bool> IsUserInRoleAsync
     } 
 }
