d18bc9c5310152b83f2838d2181bfd88ef34e717
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-09 12:59:02 -0800

    👔Add finalize  get bt roles services

--- SLMBugTracker/Services/Interfaces/IBTRolesService.cs
+++ SLMBugTracker/Services/Interfaces/IBTRolesService.cs
@@ -13,6 +13,10 @@ namespace SLMBugTracker.Services.Interfaces
      public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
      public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
      public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+     public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+     public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+     public Task<string> GetRoleNameByCompanyIdAsync(string roleId);
+
 
 
     
