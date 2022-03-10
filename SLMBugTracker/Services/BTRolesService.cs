8c0db999d3b68cd68720633ee8df09102b3da721
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-09 18:04:35 -0800

    👔Add GetUsersInRoleAsync, GetUsersNotInRoleAsync, list data sets btuser logic

--- SLMBugTracker/Services/BTRolesService.cs
+++ SLMBugTracker/Services/BTRolesService.cs
@@ -47,14 +47,18 @@ namespace SLMBugTracker.Services
 
         public async Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
         {
-            List<BTUser> users =(await _userManager.GetUsersInRoleAsync(roleName)).ToList(); 
-            List<BTUser> result = users.Where(u => u.CompanyId == companyId).ToList(); 
+            List<BTUser> users = (await _userManager.GetUsersInRoleAsync(roleName)).ToList();
+            List<BTUser> result = users.Where(u => u.CompanyId == companyId).ToList();
             return result;
         }
 
         public async Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
         {
-            throw new NotImplementedException();
+            List<string> userIds = (await _userManager.GetUsersInRoleAsync(roleName)).Select(u => u.Id).ToList();
+            List<BTUser> roleUsers = _context.Users.Where(u => !userIds.Contains(u.Id)).ToList();
+            
+            List<BTUser> result = roleUsers.Where(u => u.CompanyId == companyId).ToList();
+            return result;
         }
 
         public Task<bool> IsUserInRoleAsync(BTUser user, string roleName)
