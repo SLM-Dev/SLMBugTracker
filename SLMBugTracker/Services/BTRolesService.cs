609f22a25681ac84a4ccaa4ece7b072b3987145f
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-09 16:56:41 -0800

    👔 Add btroleservice interfacing 👔 Add identitfy and data models and naming spacing to btroleservice

--- SLMBugTracker/Services/BTRolesService.cs
+++ SLMBugTracker/Services/BTRolesService.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using SLMBugTracker.Data;
+using SLMBugTracker.Models;
+using SLMBugTracker.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services
 {
-    public class BTRolesService
+    public class BTRolesService : IBTRolesService
     {
-    
+
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<BTUser> _userManager;
+
+        public BTRolesService(ApplicationDbContext context,
+                              RoleManager<IdentityRole> roleManager, 
+                              UserManager<BTUser> userManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
