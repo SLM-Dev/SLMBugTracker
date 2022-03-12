44bc7b1c0ae4dbdda42016af253d8822c035e8fc
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-11 19:11:36 -0800

    👔Add BTCompanyInfoService interface

---
+++ SLMBugTracker/Services/BTCompanyInfoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SLMBugTracker.Services
+{
+    public class BTCompanyInfoService 
+    {
+        
+        
+    }
+}
