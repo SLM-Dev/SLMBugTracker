1526c2798d227811690948d94b945c1bcdac82fe
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-11 21:34:05 -0800

    👔Add BTCompanyInfoService implemented interface 👔 Add using services interfaces

--- SLMBugTracker/Services/BTCompanyInfoService.cs
+++ SLMBugTracker/Services/BTCompanyInfoService.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using SLMBugTracker.Models;
+using SLMBugTracker.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services
 {
-    public class BTCompanyInfoService 
+    public class BTCompanyInfoService : IBTCompanyInfoService
     {
-        
-        
+        public Task<List<BTUser>> GetAllMembersAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> GetCompanyInfoByIdAsync(int? company)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
