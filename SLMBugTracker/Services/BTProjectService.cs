50a462add607a84d347e8a9ae4f7049d3f12675a
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-13 20:52:09 -0700

    ✨ 👔 Add BTProjectService implemented interfaces and logic

--- SLMBugTracker/Services/BTProjectService.cs
+++ SLMBugTracker/Services/BTProjectService.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using SLMBugTracker.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services
 {
-    public class BTProjectService
+    public class BTProjectService : IBTProjectService
     {
+        public async Task AddNewProjectAsync(Project project)
+        {
+
+        }
+
+        public async Task<bool> AddNewProjectAsync(string userId, int projectId)
+        {
+
+        }
     }
+
 }
