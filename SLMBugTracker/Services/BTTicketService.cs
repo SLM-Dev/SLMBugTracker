8693eb1419af02a33ce98945c8a1015e83f025aa
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-16 12:21:48 -0700

    👔 Add BTTicketService created dependacy injection for btticketserivce with applicationdbcontext

--- SLMBugTracker/Services/BTTicketService.cs
+++ SLMBugTracker/Services/BTTicketService.cs
@@ -11,7 +11,7 @@ namespace SLMBugTracker.Services
 {
     public class BTTicketService : IBTTicketService
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;   
 
         public BTTicketService (ApplicationDbContext context)
         {
