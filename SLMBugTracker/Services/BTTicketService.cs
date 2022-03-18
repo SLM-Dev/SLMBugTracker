59535a175ae4830da91e697e93c4a76c52a280a0
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-18 16:13:47 -0700

    👔 Add BTTicketService created logic and methods GetProjectTicketsByStatusAsync

--- SLMBugTracker/Services/BTTicketService.cs
+++ SLMBugTracker/Services/BTTicketService.cs
@@ -222,9 +222,20 @@ namespace SLMBugTracker.Services
             }
         }
 
-        public Task<List<Ticket>> GetProjectTicketsByStatusAsync(string statusName, int companyId, int projectId)
+        public async Task<List<Ticket>> GetProjectTicketsByStatusAsync(string statusName, int companyId, int projectId)
         {
-            throw new NotImplementedException();
+            List<Ticket> tickets = new();
+
+            try
+            {
+                tickets = (await GetAllTicketsByStatusAsync(companyId, statusName)).Where(t => t.ProjectId == projectId).ToList();
+                return tickets;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public Task<List<Ticket>> GetProjectTicketsByTypeAsync(string typeName, int companyId, int projectId)
