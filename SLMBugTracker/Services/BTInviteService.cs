9d4c030f2c41658abc7f544d28d58b613240c91b
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 13:05:10 -0700

    👔 Add BTInviteService created  logic and methods for AnyInviteAsync

--- SLMBugTracker/Services/BTInviteService.cs
+++ SLMBugTracker/Services/BTInviteService.cs
@@ -58,10 +58,22 @@ namespace SLMBugTracker.Services
             }
         }
     
-        public Task<bool> AnyInviteAsync(Guid? token, string email, int companyId)
+        public async Task<bool> AnyInviteAsync(Guid? token, string email, int companyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                bool result = await _context.Invites.Where(i => i.CompanyId == companyId) 
+                                                    .AnyAsync(i => i.CompanyToken == token && i.InviteeEmail == email);
+
+                return result;
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
         }
+        
 
         public Task<Invite> GetInviteAsync(int inviteId, int companyId)
         {
