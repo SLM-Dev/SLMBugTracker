e703e522aa9ca781d0078e50bb1adee6ba32d7f1
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 05:56:42 -0700

    👔 Add BTEmailService creating email message body logic

--- SLMBugTracker/Services/BTEmailService.cs
+++ SLMBugTracker/Services/BTEmailService.cs
@@ -21,6 +21,26 @@ namespace SLMBugTracker.Services
         public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
         {
             MimeMessage email = new();
+
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder
+            {
+                HtmlBody = htmlMessage
+            };
+            
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+
+            await smtp.SendAsync(email);
+
+            smtp.Disconnect(true);
+
         }
     }
 }
