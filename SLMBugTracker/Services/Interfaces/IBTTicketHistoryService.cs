dfa8bb3e886a249bdd7355554ce396859efa3b96
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-18 17:49:31 -0700

    👔 Add IBTTicketHistoryService created logic for AddHistoryAsync 👔 Add IBTTicketHistoryService created using namespace models

--- SLMBugTracker/Services/Interfaces/IBTTicketHistoryService.cs
+++ SLMBugTracker/Services/Interfaces/IBTTicketHistoryService.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using SLMBugTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services.Interfaces
 {
-    interface IBTTicketHistoryService
+    public interface IBTTicketHistoryService
     {
-    }
+     Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+    } 
 }
