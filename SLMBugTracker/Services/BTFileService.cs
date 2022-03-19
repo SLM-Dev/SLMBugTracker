1a88f83cc3f9113dd125d6e27b0e55dad891ab81
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 14:19:09 -0700

    👔 Add BTFileService created  ConvertFileToByteArrayAsync methods and logic 👔 Add BTFileService created  system.io 👔 Add BTFileService created  memory stream 👔 Add BTFileService created  logic to save to database

--- SLMBugTracker/Services/BTFileService.cs
+++ SLMBugTracker/Services/BTFileService.cs
@@ -2,6 +2,7 @@
 using SLMBugTracker.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,10 +28,24 @@ namespace SLMBugTracker.Services
         }
 
 
-        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
         {
-            throw new NotImplementedException();
+            try
+            {
+                MemoryStream memoryStream = new ();
+                await file.CopyToAsync(memoryStream); 
+                byte[] byteFile = memoryStream.ToArray(); 
+                memoryStream.Close(); 
+                memoryStream.Dispose(); 
+                return byteFile;
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
         }
+       
 
         public string FormatFileSize(long bytes)
         {
