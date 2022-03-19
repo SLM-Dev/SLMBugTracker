af6c950636cd1984003f4238b773012da403f496
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 07:32:35 -0700

    👔 Add IBTNotificationService created method and logic for SendEmailNotificationAsync

--- SLMBugTracker/Services/Interfaces/IBTNotificationService.cs
+++ SLMBugTracker/Services/Interfaces/IBTNotificationService.cs
@@ -14,6 +14,8 @@ namespace SLMBugTracker.Services.Interfaces
         public Task<List<Notification>> GetSentNotificationsAsync(string userId);
         public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role);
         public Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members);
+        public Task SendEmailNotificationAsync(Notification notification, string emailSubject);
+
 
 
 
