eff5155165df4d3d3b7018f84b729dee50adf6b1
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 14:01:49 -0700

    👔 Add Interface created IBTFileService interface service 👔 Add IBTFileService created methods 👔 Add IBTFileService created aspcore namespace

--- SLMBugTracker/Services/Interfaces/IBTFileService.cs
+++ SLMBugTracker/Services/Interfaces/IBTFileService.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services.Interfaces
 {
-    interface IBTFileService
+    public interface IBTFileService
     {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+        public string GetFileIcon(string file);
+        public string FormatFileSize(long bytes);
     }
 }
