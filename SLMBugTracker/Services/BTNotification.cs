cb28e23bf77c53464b295160c6a47539ec507022
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 10:12:05 -0700

    👔 Add BTNotificationService created logic and methods for SendEmailNotificationsByRoleAsync 👔 Add BTNotificationService created communcation between SendEmailNotificationsByRoleAsync,SendEmailNotificationAsync and GetUsersInRoleAsync

--- SLMBugTracker/Services/BTNotification.cs
+++ SLMBugTracker/Services/BTNotification.cs
@@ -108,10 +108,25 @@ namespace SLMBugTracker.Services
                 return false;
             }
         }
-        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role)
+        public async Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role)
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<BTUser> members = await _rolesService.GetUsersInRoleAsync(role, companyId);
+
+                foreach (BTUser btUser in members)
+                {
+                    notification.RecipientId = btUser.Id;
+                    await SendEmailNotificationAsync(notification, notification.Title);
+                }
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
         }
+
 
         public Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members)
         {
