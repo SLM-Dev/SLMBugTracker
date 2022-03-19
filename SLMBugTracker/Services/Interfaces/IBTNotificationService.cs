e1dfd59f22015e4f0ed37e90ced5dd923303af7d
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 07:13:18 -0700

    👔 Add IBTNotificationService created using space models
    👔 Add IBTNotificationService created notification for database

--- SLMBugTracker/Services/Interfaces/IBTNotificationService.cs
+++ SLMBugTracker/Services/Interfaces/IBTNotificationService.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using SLMBugTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services.Interfaces
 {
-    interface IBTNotificationService
+    public interface IBTNotificationService
     {
+        public Task AddNotificationAsync(Notification notification);
     }
 }
