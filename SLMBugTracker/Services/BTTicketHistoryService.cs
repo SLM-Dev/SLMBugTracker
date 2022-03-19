dbff3a239a46f15e44732b9ab47988965a5e6a1e
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-18 18:00:03 -0700

    👔 Add BTTicketHistoryService implemented interface for BTTicketHistoryService

--- SLMBugTracker/Services/BTTicketHistoryService.cs
+++ SLMBugTracker/Services/BTTicketHistoryService.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using SLMBugTracker.Models;
+using SLMBugTracker.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services
 {
-    public class BTTicketHistoryService
+    public class BTTicketHistoryService : IBTTicketHistoryService
     {
+        public Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
