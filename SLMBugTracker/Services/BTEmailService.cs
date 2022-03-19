506d3c98b3c3e539f2c05be29d6e72a04005479a
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 05:19:26 -0700

    👔 Add EmailService created BTEmailService using name space aspnetcore identity ui services

--- SLMBugTracker/Services/BTEmailService.cs
+++ SLMBugTracker/Services/BTEmailService.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services
 {
-    public class BTEmailService
+    public class BTEmailService : IEmailSender
     {
     }
 }
