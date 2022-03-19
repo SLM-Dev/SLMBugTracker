ce23114907372860bb229895636c1f01c4533438
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 12:26:07 -0700

    👔 Add BTInviteService modify logic and methods for BTInviteService 👔 Add BTInviteService created dependancy injection

--- SLMBugTracker/Services/BTInviteService.cs
+++ SLMBugTracker/Services/BTInviteService.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿using SLMBugTracker.Data;
+using SLMBugTracker.Models;
+using SLMBugTracker.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services
 {
-    public class BTInviteService
+    public class BTInviteService : IBTInviteService
     {
+        private readonly ApplicationDbContext _context;
+
+        public BTInviteService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddNewInviteAsync(Invite invite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyInviteAsync(Guid? token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(int inviteId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateInviteCodeAsync(Guid? token)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
