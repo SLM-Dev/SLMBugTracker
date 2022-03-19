b019769605aba8e44e3e581ef30388a448202dc9
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-18 17:51:16 -0700

    👔 Add IBTTicketHistoryService created logic for GetProjectTicketsHistoriesAsync

--- SLMBugTracker/Services/Interfaces/IBTTicketHistoryService.cs
+++ SLMBugTracker/Services/Interfaces/IBTTicketHistoryService.cs
@@ -8,6 +8,8 @@ namespace SLMBugTracker.Services.Interfaces
 {
     public interface IBTTicketHistoryService
     {
-     Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+     Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId);
+
+     Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId);
     } 
 }
