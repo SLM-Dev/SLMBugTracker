acdbb157812dca149b05aa073e4f35e7ddd43b3a
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-18 21:53:00 -0700

    👔 Add BTTicketHistoryService created logic for GetCompanyTicketsHistoriesAsync ✏️ Fix BTTicketHistoryService typos

--- SLMBugTracker/Services/BTTicketHistoryService.cs
+++ SLMBugTracker/Services/BTTicketHistoryService.cs
@@ -1,4 +1,5 @@
-﻿using SLMBugTracker.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SLMBugTracker.Data;
 using SLMBugTracker.Models;
 using SLMBugTracker.Services.Interfaces;
 using System;
@@ -6,34 +7,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SLMBugTracker.Services
-{
+namespace SLMBugTracker.Services;
+
     public class BTTicketHistoryService : IBTTicketHistoryService
+{
+    private readonly ApplicationDbContext _context;
+
+    public BTTicketHistoryService(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public BTTicketHistoryService(ApplicationDbContext context)
+
+    public async Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+    {
+        // NEW TICKET HAS BEEN ADDED
+        if (oldTicket == null && newTicket != null)
+
         {
-            _context = context;
-        }
 
-
-        public async Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
-        {
-            // NEW TICKET HAS BEEN ADDED
-            if (oldTicket == null && newTicket != null)
-            else
+            TicketHistory history = new()
             {
-
-                TicketHistory history = new()
-                { 
-                    TicketId = newTicket.Id,
-                    Property = "",
-                    OldValue = "",
-                    NewValue = "",
-                    Created = DateTimeOffset.Now,
-                    UserId = userId,
-                    Description = "New Ticket Created"
+                TicketId = newTicket.Id,
+                Property = "",
+                OldValue = "",
+                NewValue = "",
+                Created = DateTimeOffset.Now,
+                UserId = userId,
+                Description = "New Ticket Created"
             };
 
             try
@@ -109,14 +110,14 @@ namespace SLMBugTracker.Services
                 };
                 await _context.TicketHistories.AddAsync(history);
             }
-    
+
             // Check Ticket Type
             if (oldTicket.TicketTypeId != newTicket.TicketTypeId)
             {
                 TicketHistory history = new()
                 {
                     TicketId = newTicket.Id,
-                    Property = "TicketType",
+                    Property = "TicketTypeId",
                     OldValue = oldTicket.TicketType.Name,
                     NewValue = newTicket.TicketType.Name,
                     Created = DateTimeOffset.Now,
@@ -125,17 +126,62 @@ namespace SLMBugTracker.Services
                 };
                 await _context.TicketHistories.AddAsync(history);
             }
-            
+            // Check Ticket Developer
+            if (oldTicket.DeveloperUserId != newTicket.DeveloperUserId)
+            {
+                TicketHistory history = new()
+                {
+                    TicketId = newTicket.Id,
+                    Property = "Developer",
+                    OldValue = oldTicket.DeveloperUser?.FullName ?? "Not Assigned",
+                    NewValue = newTicket.DeveloperUser?.FullName,
+                    Created = DateTimeOffset.Now,
+                    UserId = userId,
+                    Description = $"New ticket developer: {newTicket.DeveloperUser.FullName}"
+                };
+                await _context.TicketHistories.AddAsync(history);
+            }
+            try
+            // Save the TicketHistory DataBaseSet to the database
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
 
 
-    public Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId)
+
+    public async Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId)
     {
-        throw new NotImplementedException();
+        try
+        {
+            List<Project> projects = (await _context.Companies
+                                                    .Include(c => c.Projects)
+                                                    .ThenInclude(p => p.Tickets)
+                                                    .ThenInclude(t => t.History)
+                                                    .ThenInclude(h => h.User)
+                                                    .FirstOrDefaultAsync(c => c.Id == companyId)).Projects.ToList();
+            List<Ticket> tickets = projects.SelectMany(p => p.Tickets).ToList();
+            List<TicketHistory> ticketHistories = tickets.SelectMany(t => t.History).ToList();
+            return ticketHistories;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
     }
 
     public Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
     {
         throw new NotImplementedException();
     }
+}
 }
 
--- SLMBugTracker/Services/Interfaces/IBTTicketHistoryService.cs
+++ SLMBugTracker/Services/Interfaces/IBTTicketHistoryService.cs
@@ -9,7 +9,7 @@ namespace SLMBugTracker.Services.Interfaces
     public interface IBTTicketHistoryService
     {
      Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId);
-
+     
      Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId);
 
      Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId);
