eae79e2c44448902a1e2cfb709d582652de19b97
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-20 14:22:29 -0700

    👔 Add AddProjectWithPMViewModel created logic and methods 👔 Add AddProjectWithPMViewModel created rendering namespace

--- SLMBugTracker/Models/ViewModels/AddProjectWithPMViewModel.cs
+++ SLMBugTracker/Models/ViewModels/AddProjectWithPMViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace SLMBugTracker.Models.ViewModels
 {
     public class AddProjectWithPMViewModel
     {
+        public Project Project { get; set; }
+        public SelectList PMList { get; set; }
+        public string PmId { get; set; }
+        public SelectList PriorityList { get; set; }
+        public int ProjectPriority { get; set; }
+        
     }
 }
