5f8fd6926c25f0fa736b0c53ee1a16114e87183f
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-20 13:38:34 -0700

    👔 Add Services created BTLookupService

--- SLMBugTracker/Services/BTLookupService.cs
+++ SLMBugTracker/Services/BTLookupService.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using SLMBugTracker.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services
 {
-    public class BTLookupService
+    public class BTLookupService : IBTLookupService
     {
     }
 }
