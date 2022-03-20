986c53224d08a6de9c154683217b93ffa51dec9f
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-20 13:37:59 -0700

    ✏️ Fix typos IBTLookupService

--- SLMBugTracker/Services/Interfaces/IBTLookupService.cs
+++ SLMBugTracker/Services/Interfaces/IBTLookupService.cs
@@ -8,8 +8,8 @@ namespace SLMBugTracker.Services.Interfaces
 {
     public interface IBTLookupService
     {
-        public interface IBTLookupService
-        {
+       
+        
             public Task<List<TicketPriority>> GetTicketPrioritiesAsync();
             public Task<List<TicketStatus>> GetTicketStatusesAsync();
             public Task<List<TicketType>> GetTicketTypesAsync();
@@ -17,4 +17,4 @@ namespace SLMBugTracker.Services.Interfaces
 
         }
     }
-}
+
