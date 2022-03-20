43fb670c513ba46a2414e1aac9e58aa02093a381
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-20 13:39:18 -0700

    👔 Add BTLookupService created logic and methods

--- SLMBugTracker/Services/BTLookupService.cs
+++ SLMBugTracker/Services/BTLookupService.cs
@@ -1,4 +1,5 @@
-﻿using SLMBugTracker.Services.Interfaces;
+﻿using SLMBugTracker.Models;
+using SLMBugTracker.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,24 @@ namespace SLMBugTracker.Services
 {
     public class BTLookupService : IBTLookupService
     {
+        public Task<List<ProjectPriority>> GetProjectPrioritiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketPriority>> GetTicketPrioritiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketStatus>> GetTicketStatusesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketType>> GetTicketTypesAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
