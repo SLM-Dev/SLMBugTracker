f580c503bbc136e96c5994df31743fe2b1ffc9be
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 23:19:20 -0700

    👔 Add BTUserClaimsPrincipalFactory created methods and logic for BTUserClaimsPrincipalFactory 👔 Add BTUserClaimsPrincipalFactory created name space for claims 👔 Add BTUserClaimsPrincipalFactory created name space for Options 👔 Add BTUserClaimsPrincipalFactory created communication between GenerateClaimsAsync, ClaimsIdentity and BTUserClaimsPrincipalFactory

--- SLMBugTracker/Services/Factories/BTUserClaimsPrincipalFactory.cs
+++ SLMBugTracker/Services/Factories/BTUserClaimsPrincipalFactory.cs
@@ -1,13 +1,32 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 using SLMBugTracker.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services.Factories
 {
     public class BTUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>
     {
+    
+        public BTUserClaimsPrincipalFactory(UserManager<BTUser> userManager, 
+                                    RoleManager<IdentityRole> roleManager, 
+                                    IOptions<IdentityOptions> optionsAccessor) 
+            : base(userManager, roleManager, optionsAccessor)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(BTUser user)
+        {
+            ClaimsIdentity identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("CompanyId", user.CompanyId.ToString()));
+
+            return identity;
+        }
     }
-}
+    
+    }
+
