d1791c3910ac38dd12d9c5ff4dba1e09bc89cd18
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-19 23:09:56 -0700

    👔 Add BTUserClaimsPrincipalFactory created using name space models

--- SLMBugTracker/Services/Factories/BTUserClaimsPrincipalFactory.cs
+++ SLMBugTracker/Services/Factories/BTUserClaimsPrincipalFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using SLMBugTracker.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SLMBugTracker.Services.Factories
 {
-    public class BTUserClaimsPrincipalFactory : UserClaimsPrincipalFactory
+    public class BTUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>
     {
     }
 }
