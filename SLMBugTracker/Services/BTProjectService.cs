325677966a5ce8f80e553f8ebc77535b98b2f9ff
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-23 22:40:56 -0700

    👔 Add BTProjectService creating methods and logic for getarchievedprojectsbycompany

--- SLMBugTracker/Services/BTProjectService.cs
+++ SLMBugTracker/Services/BTProjectService.cs
@@ -174,6 +174,7 @@ namespace SLMBugTracker.Services
         public async Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName)
         {
             List<Project> projects = await GetAllProjectsByCompanyAsync(companyId);
+            
             int projectId = await LookupProjectPriorityId(priorityName);
 
             return projects.Where(p => p.ProjectPriorityId == projectId).ToList();
@@ -182,9 +183,38 @@ namespace SLMBugTracker.Services
 
         public async Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
         {
-            List<Project> projects = await GetAllProjectsByCompanyAsync(companyId);
+            try
+            {
+                List<Project> projects = await _context.Projects.Where(p => p.CompanyId == companyId && p.Archived == true)
+                                             .Include(p => p.Members)
+                                             .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.Comments)
+                                             .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.Attachments)
+                                             .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.History)
+                                             .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.Notifications)
+                                             .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.DeveloperUser)
+                                             .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.OwnerUser)
+                                             .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.TicketStatus)
+                                             .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.TicketPriority)
+                                             .Include(p => p.Tickets)
+                                                 .ThenInclude(t => t.TicketType)
+                                             .Include(p => p.ProjectPriority)
+                                             .ToListAsync();
 
-            return projects.Where(p => p.Archived == true).ToList();
+                return projects;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public Task<List<BTUser>> GetDevelopersOnProjectAsync(int projectId)
