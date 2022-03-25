4a12e909901c108e6a15a8df7cef777c3de17806
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-25 14:05:21 -0700

    👔 Add TicketsController created method and logic for Restore

--- SLMBugTracker/Controllers/TicketsController.cs
+++ SLMBugTracker/Controllers/TicketsController.cs
@@ -241,6 +241,48 @@ namespace SLMBugTracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        // GET: Tickets/Restore/5
+        public async Task<IActionResult> Restore(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //var ticket = await _context.Tickets
+            //    .Include(t => t.DeveloperUser)
+            //    .Include(t => t.OwnerUser)
+            //    .Include(t => t.Project)
+            //    .Include(t => t.TicketPriority)
+            //    .Include(t => t.TicketStatus)
+            //    .Include(t => t.TicketType)
+            //    .FirstOrDefaultAsync(m => m.Id == id);
+
+
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id.Value);
+
+
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // POST: Tickets/Restore/5
+        [HttpPost, ActionName("Restore")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RestoreConfirmed(int id)
+        {
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id);
+            ticket.Archived = false;
+            await _ticketService.UpdateTicketAsync(ticket);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
         private async Task <bool> TicketExists(int id)
         {
             int companyId = User.Identity.GetCompanyId().Value;
