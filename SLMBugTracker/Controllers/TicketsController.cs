4619a5cbb2190ccbba4e010a4ce3430ff5be28dd
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-03-28 11:24:13 -0700

    ✨ 👔 Add TicketController  created logic for Getuserasync

--- SLMBugTracker/Controllers/TicketsController.cs
+++ SLMBugTracker/Controllers/TicketsController.cs
@@ -39,6 +39,11 @@ namespace SLMBugTracker.Controllers
         public async Task<IActionResult> Index()
 
         public async Task<IActionResult> MyTickets()
+        {
+            BTUser btUser = await _userManager.GetUserAsync(User);
+
+            List<Ticket> tickets =
+        }
 
         {
             var applicationDbContext = _context.Tickets.Include(t => t.DeveloperUser).Include(t => t.OwnerUser).Include(t => t.Project).Include(t => t.TicketPriority).Include(t => t.TicketStatus).Include(t => t.TicketType);
