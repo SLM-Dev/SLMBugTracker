7177060ed2440c4c173a8df19357c3ce89271ec3
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-04-04 01:39:04 -0700

    👔 Add ViewModels created methods for AssignDeveloperViewmodel

--- SLMBugTracker/Models/ViewModels/AssignDeveloperViewModel.cs
+++ SLMBugTracker/Models/ViewModels/AssignDeveloperViewModel.cs
@@ -7,5 +7,8 @@ namespace SLMBugTracker.Models.ViewModels
 {
     public class AssignDeveloperViewModel
     {
+        public Ticket Ticket { get; set; }
+        public SelectList Developers { get; set; }
+        public string DeveloperId { get; set; }
     }
 }
