f4e26a10448a27a6f8f592f7a732bef68495923b
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-04-08 02:53:30 -0700

    👔 Add AssignPM created logic and methods for  AssignPM PMViewmodel

--- SLMBugTracker/Controllers/ProjectsController.cs
+++ SLMBugTracker/Controllers/ProjectsController.cs
@@ -119,8 +119,15 @@ namespace SLMBugTracker.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AssignPM(AssignPMViewModel model)
         {
+            if (!string.IsNullOrEmpty(model.PMID))
+            {
+                await _projectService.AddProjectManagerAsync(model.PMID, model.Project.Id);
+                return RedirectToAction(nameof(Details), new {id = model.Project.Id});
+            }
 
+            return RedirectToAction(nameof(AssignPM), new { projectId = model.Project.Id });
         }
+        
 
 
         // GET: Projects/Details/5
