ff44b76afaec16d68bffc4f0e61bb94c864b536e
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-04-09 11:20:05 -0700

    👔 AddI BTTicketHistoryService created method as no tracking entity frame work for  GetTicketAsNoTrackingAsync

--- SLMBugTracker/Services/Interfaces/IBTTicketService.cs
+++ SLMBugTracker/Services/Interfaces/IBTTicketService.cs
@@ -23,6 +23,8 @@ namespace SLMBugTracker.Services.Interfaces
         public Task<List<Ticket>> GetAllTicketsByPriorityAsync(int companyId, string priorityName);
         public Task<List<Ticket>> GetAllTicketsByStatusAsync(int companyId, string statusName);
         public Task<List<Ticket>> GetAllTicketsByTypeAsync(int companyId, string typeName);
+        public Task<Ticket> GetTicketAsNoTrackingAsync(int ticketId);
+        
         public Task<TicketAttachment> GetTicketAttachmentByIdAsync(int ticketAttachmentId);
          
         public Task<BTUser> GetTicketDeveloperAsync(int ticketId, int companyId);
