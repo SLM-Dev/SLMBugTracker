e4ac2bdfb5e65cf59ce6e4dd77ee1c2bb53a46b9
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-04-11 09:10:59 -0700

    👔 Add DashboardViewModel created using namespace view models  for view model dashboard

--- SLMBugTracker/Controllers/HomeController.cs
+++ SLMBugTracker/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using SLMBugTracker.Extensions;
 using SLMBugTracker.Models;
+using SLMBugTracker.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
