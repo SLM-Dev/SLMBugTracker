a5f68c126097b296772ff28c82fe22c76516ebd2
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-04-11 09:07:23 -0700

    👔 Add DashboardViewModel created methods for dashboard

--- SLMBugTracker/Models/ViewModels/DashboardViewModel.cs
+++ SLMBugTracker/Models/ViewModels/DashboardViewModel.cs
@@ -7,5 +7,9 @@ namespace SLMBugTracker.Models.ViewModels
 {
     public class DashboardViewModel
     {
+        public Company Company { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<BTUser> Members { get; set; }
     }
 }
